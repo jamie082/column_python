fb5661c2360ba7f4d74141fb4e5e594f7eabd069
jamie082 <jamiemorrissey02@gmail.com>
2021-09-18 21:39:59 +0000

    fixed for loop on second column

--- column_python.py
+++ column_python.py
@@ -6,18 +6,36 @@
 digit_1 = 1 # #starting sequence
 print('\n')
 
-num = int(input("Insert num: "))
-print ('\n')
-
-digit_2 = 2  # starting sequence
-print("Column #1")
-for x in range (digit_1, num, 3): # have num multiple of 3 
+num = int(input("Lines of output: "))
+print('\n')
+    
+mod_x = 3  # starting sequence
+print("Modulus % 3 (3)")
+for x in range (digit_1, 3): # have num multiple of 3 
     if num % 3 == 0:
         print("We're on time %d" % (x))
 
+    else:
+       # print("* ERROR %d" % (x))
+       break
+
 print ("\n")
 
-print ('Column #2')
-for y in range (digit_2, num, 2): # have num multiple of 2
-    if num % 2 == 0:
-        print ("We're on time %d" % (y))
+
+# second column
+p = 0
+digit_2 = 2
+print ('Modulus % 2 (2)')
+print ("The factors of ", digit_2, "are: ")
+for y in range (num): # line count numbers, first loop
+    for p in range (digit_2, num, 10): # second loop
+        if num % 2 == 0:
+            print ("We're on time %d" % (p))
+
+        else:
+            print("Not a % of 2\n")
+
+print ('\n')
+print ('Column #3 (LIST NUMBER)')
+for p in range (1, num, 1):
+    print ("We're on time %d" % (p))
